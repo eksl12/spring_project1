39f17cb83926c1e736d0799cb6b88eeab878f428
zodaland <ekwhwhek@gmail.com>
2021-05-04 17:53:39 +0900

    [java] 기본 자바 파일 변경

--- app/src/main/java/test/gradle/App.java
+++ app/src/main/java/test/gradle/App.java
@@ -2,23 +2,11 @@ package test.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-	@RequestMapping("/test")
-	public String test() {
-		return "test";
-	}
-
-    @RequestMapping("/")
-    public String hello() {
-        return "hello";
     }
 }
 
