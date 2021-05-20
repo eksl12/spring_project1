28b32d6bbbd7e46875a66b3862dd4ba87e31c4b9
zodaland <ekwhwhek@gmail.com>
2021-05-20 17:46:53 +0900

    [configuration] tiles layout 경로 설정 및 static경로 설정 클래스 파일 추가

---
+++ app/src/main/java/test/gradle/configuration/WebMvcConfiguration.java
@@ -0,0 +1,28 @@
+package test.spring.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler( "/static/**" ).addResourceLocations( "classpath:/static/");
+    }
+}
