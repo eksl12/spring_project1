cba1c0b04a3ba0e518e7b1616253ab2a25823897
zodaland <ekwhwhek@gmail.com>
2021-05-20 17:48:08 +0900

    [controller] 대시보드 컨트롤러 클래스 추가

---
+++ app/src/main/java/test/gradle/controller/dashboard/Dashboard.java
@@ -0,0 +1,22 @@
+package test.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Dashboard {
+
+    private static final String VIEW_PATH = "dashboard/";
+
+    @RequestMapping( value = "/dashboard", method = RequestMethod.GET )
+    public String dashboard(Model model) {
+        return VIEW_PATH + "dashboard";
+    }
+
+    @RequestMapping( value = "/dashboard2", method = RequestMethod.GET )
+    public String dashboard2(Model model) {
+        return VIEW_PATH + "dashboard2";
+    }
+}
